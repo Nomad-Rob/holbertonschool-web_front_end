5f31968f60db9e19164d69d77db264cb55fff377
Nomad-Rob <rfarley89@gmail.com>
2023-11-13 13:27:04 -0600

    adding task 5 completed

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -21,38 +21,38 @@ JQuery not allowed!
 */
 
 function changeMode(size, weight, transform, background, color) {
-    return function() {
-        document.body.style.fontSize = size + 'px';
-        document.body.style.fontWeight = weight;
-        document.body.style.textTransform = transform;
-        document.body.style.backgroundColor = background;
-        document.body.style.color = color;
-    }
+  return function() {
+    document.body.style.fontSize = size + 'px';
+    document.body.style.fontWeight = weight;
+    document.body.style.textTransform = transform;
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
+  };
 }
 
 function main() {
-  var spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
-  var darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
-  var screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
+  let spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+  let darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
+  let screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
   
-  var paragraph = document.createElement('p');
-  paragraph.innerHTML = 'Welcome Holberton!';
-  document.body.appendChild(paragraph);
+  let p = document.createElement('p');
+  p.textContent = 'Welcome Holberton!';
+  document.body.appendChild(p);
   
-  var buttonSpooky = document.createElement('button');
-  buttonSpooky.innerHTML = 'Spooky';
-  buttonSpooky.onclick = spooky;
-  document.body.appendChild(buttonSpooky);
+  let spookyButton = document.createElement('button');
+  spookyButton.textContent = 'Spooky';
+  spookyButton.onclick = spooky;
+  document.body.appendChild(spookyButton);
   
-  var buttonDarkMode = document.createElement('button');
-  buttonDarkMode.innerHTML = 'Dark mode';
-  buttonDarkMode.onclick = darkMode;
-  document.body.appendChild(buttonDarkMode);
+  let darkButton = document.createElement('button');
+  darkButton.textContent = 'Dark mode';
+  darkButton.onclick = darkMode;
+  document.body.appendChild(darkButton);
   
-  var buttonScreamMode = document.createElement('button');
-  buttonScreamMode.innerHTML = 'Scream mode';
-  buttonScreamMode.onclick = screamMode;
-  document.body.appendChild(buttonScreamMode);
+  let screamButton = document.createElement('button');
+  screamButton.textContent = 'Scream mode';
+  screamButton.onclick = screamMode;
+  document.body.appendChild(screamButton);
 }
 
 main();
